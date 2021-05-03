30e7a1783a2206b46f40950afa3645189af53a96
Maciej Krok <inz.krok.maciej@gmail.com>
2021-05-03 13:51:29 +0200

    Project 6/6

---
+++ SimpleBot.java
@@ -0,0 +1,15 @@
+package bot;
+
+public class SimpleBot {
+
+    public static void main(String[] args) {
+        Bot.greet("Aid", "2021");
+        Bot.remindName();
+        Bot.guessAge();
+        Bot.count();
+        Bot.test();
+        Bot.end();
+    }
+
+
+}
